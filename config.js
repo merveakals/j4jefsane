bfd765d79bf12712daf7000dddb15b4b490c3bdc
Merveakals <59118776+merveakals@users.noreply.github.com>
2023-02-03 18:49:37 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  tokens: ["Token Yapıştır"],
+  channels: ["745910695991967834", "934208012623679498", "987201947574616155", "1004507827471138928", "703618624421888020", "1032289427034210354", "1028716461004439643", "1025698887694036993"],
+  messages: {
+    dm: {
+      first: [ "j4j dM https://discord.gg/cJG9jVPtbB", "Join J4j https://discord.gg/cJG9jVPtbB", "jJ4 https://discord.gg/cJG9jVPtbB" ],
+      end: [ "bDone Verifed! If you don't write J4J i will Exit", "If you don't write J4J i will Exit", "**Done Verifed!** If you don't write J4J i will Exit" ]
+    },
+    server: [ "j4J Dm", "Dm J4j Fast", "Fast Dm J4j", "J4j Not Bots", "J.4J No Bot" ],
+    nobot: [ "No Bot Bro", "No Bot Bruhhh", "No No I' m no bot" ]
+  },
+  systems: {
+    /* Maintence Modes */
+    server: true,
+    dm: true,
+    randomdm: true
+  },
+  timeouts: {
+    /* Please No Edit! */
+    randomdm: 2 * 60 * 1000,
+    dmad: 2 * 60 * 60 * 1000,
+    server: 10 * 1000,
+    
+    /* For Discord Ban */
+    stoptime: 10,
+    starttime: 5,
+    maxdmab: 7
+  },
+   // No Edit!!!
+  secret: "104688341102319b385fd64331eb3055"
+}
